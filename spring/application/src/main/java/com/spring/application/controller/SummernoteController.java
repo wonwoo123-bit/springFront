61828c7dbb8f9e90dc0464e49f8562c42580b0c0
wonwoo123-bit <sww0318@gmail.com>
2025-09-10 13:43:19 +0900

    4

--- spring/application/src/main/java/com/spring/application/controller/SummernoteController.java
+++ spring/application/src/main/java/com/spring/application/controller/SummernoteController.java
@@ -1,12 +1,17 @@
 package com.spring.application.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +41,41 @@ public record SummernoteController(@Value("${summernote.img}") String imgPath) {
 				+ "/summernote/getImg?fileName=" + fileName,HttpStatus.OK);
 		return result;
 	}
+
+    @GetMapping("/getImg")
+    public ResponseEntity<byte[]> getImg(String fileName) throws Exception{
+        
+        ResponseEntity<byte[]> entity = null;
+
+        String savePath = imgPath.replace("/",File.separator);
+        File sendFile = new File(savePath, fileName);
+
+        InputStream in = null;
+        try{
+            in = new FileInputStream(sendFile);
+            entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in),HttpStatus.OK);
+        } finally{
+            if (in != null)
+            in.close();
+        }
+        return entity;
+    }
+
+    @GetMapping("/deleteImg")
+    public ResponseEntity<String> deleteImg(String fileName){
+        ResponseEntity<String> result = null;
+
+        String savePath= imgPath.replace("/",File.separator);
+        File target = new File(savePath, fileName);
+
+        if (!target.exists()){
+            result = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }else{
+            target.delete();
+            result = new ResponseEntity<String>(fileName, HttpStatus.OK);
+        }
+        return result;
+    }
 
 
 }
